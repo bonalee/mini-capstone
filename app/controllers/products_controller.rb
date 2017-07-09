1c4870c15e4a0b24c0e0ca2c3b6599c2400ccc3f
Bona Lee <bonalee3@gmail.com>
2017-07-09 11:43:18 -0500

    Got the show page to work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,9 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
-  def info
-    donut_info = params[:name]
-    @donut = Product.find_by(id: recipe_id)
+  def donut_description
+    donut_id = params[:id]
+    @donut = Product.find_by(id: donut_id)
     render "info.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products" => "products#donut_index"
-  get "/products/:info" => "products#info"
+  get "/products/:id" => "products#donut_description"
 end

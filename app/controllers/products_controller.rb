8a0b7a34ac46c2d17411bd1f8afca397ac34adeb
Bona Lee <bonalee3@gmail.com>
2017-07-10 20:13:56 -0500

    Adding more donuts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,19 @@ class ProductsController < ApplicationController
     @donut = Product.find_by(id: donut_id)
     render "info.html.erb"
   end
+
+  def donut_creation
+    donut = Product.new(
+      name: params[:form_name],
+      price: params[:form_price],
+      image: params[:form_image],
+      description: params[:form_description]
+      )
+    donut.save
+    render "new_donut.html.erb"
+  end
+
+  def new_donut
+    render "request_creation.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products" => "products#donut_index"
+  get "/products/new" => "products#new_donut"
+  post "/products/" => "products#donut_creation"
   get "/products/:id" => "products#donut_description"
 end
